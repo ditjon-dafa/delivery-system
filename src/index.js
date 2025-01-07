5255978c9acd4c76efabbca20051e525316518ad
Ditjon <dafaditjon@gmail.com>
2025-01-07 19:18:30 +0100

    Added Show location button with its functionality and style.

---
+++ src/index.js
@@ -0,0 +1,35 @@
+let btnShowLocation = document.getElementById("location");
+btnShowLocation.addEventListener("click", showLocation);
+
+function showLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  let latitudeInput = document.getElementById("lat");
+  latitudeInput.value = position.coords.latitude;
+
+  let longituteInput = document.getElementById("long");
+  longituteInput.value = position.coords.longitude;
+}
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.");
+      break;
+  }
+}
